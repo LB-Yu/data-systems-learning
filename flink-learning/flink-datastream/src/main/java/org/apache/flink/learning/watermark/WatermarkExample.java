8cbdc59fbf4f4978e3e9f15d460dff81f1c3d298
LB-Yu <liebingyu@gmail.com>
2021-12-20 17:34:28 +0800

    add

---
+++ flink-learning/flink-datastream/src/main/java/org/apache/flink/learning/watermark/WatermarkExample.java
@@ -0,0 +1,12 @@
+package org.apache.flink.learning.watermark;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WatermarkExample {
+
+  public static void main(String[] args) {
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+  }
+}
---
+++ flink-learning/flink-sql/src/main/java/org/apache/flink/learning/table/utils/Shipment.java
@@ -0,0 +1,48 @@
+package org.apache.flink.learning.table.utils;
+
+public class Shipment {
+
+  private String shipId;
+  private String orderId;
+  private String company;
+  private long timestamp;
+
+  public Shipment(String shipId, String orderId, String company, long timestamp) {
+    this.shipId = shipId;
+    this.orderId = orderId;
+    this.company = company;
+    this.timestamp = timestamp;
+  }
+
+  public String getShipId() {
+    return shipId;
+  }
+
+  public void setShipId(String shipId) {
+    this.shipId = shipId;
+  }
+
+  public String getOrderId() {
+    return orderId;
+  }
+
+  public void setOrderId(String orderId) {
+    this.orderId = orderId;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+}
